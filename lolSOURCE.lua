55b25ef730b02c954583b25e11a96cb06384225f
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-13 20:20:53 -0400

    Update lolSOURCE.lua

--- lolSOURCE.lua
+++ lolSOURCE.lua
@@ -1,5 +1,4 @@
 game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
-wait(1.2)
 firesignal(game.ReplicatedStorage.EntityInfo.DeathHint.OnClientEvent, {"LSPLASH is gonna jiggle and tickle your balls.", "Let that cummie come out.", "Lemme drink that tasty cum", "I shit out my dick btw"}, "fatass") end)
 game.Players.LocalPlayer.Character.Humanoid.Health = 0
 wait(15)
