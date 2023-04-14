e4ded8de80fedee136f61438e862a4ef59a2ec24
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-13 20:15:20 -0400

    Create lolSOURCE.lua

---
+++ lolSOURCE.lua
@@ -0,0 +1,6 @@
+game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
+wait(1.2)
+firesignal(game.ReplicatedStorage.EntityInfo.DeathHint.OnClientEvent, {"LSPLASH is gonna jiggle and tickle your balls.", "Let that cummie come out.", "Lemme drink that tasty cum", "I shit out my dick btw"}, "fatass") end)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(15)
+game.Players.LocalPlayer:Kick("haha that cummie tasted so good lemme get some more.")
