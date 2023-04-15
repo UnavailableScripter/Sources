f42c93c692b3b9b3730ec312ec5dc9dad13c4cc5
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-15 15:09:12 -0400

    Create customDeathMessageSource.lua

---
+++ customDeathMessageSource.lua
@@ -0,0 +1,14 @@
+local customMessage = nil
+local Type = nil -- Change to Curious Light to have Curious Light layout
+local activeType = nil
+
+if Type = "Guiding Light" then
+	activateType = "Blue"
+elseif Type = "Curious Light" then
+	activateType = "Yellow"
+end
+
+game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
+wait(1.2)
+firesignal(game.ReplicatedStorage.EntityInfo.DeathHint.OnClientEvent, customMessage, activateType) end)
+wait(300)
