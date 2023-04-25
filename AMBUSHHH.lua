ed42d8e1d30bdfa883dbd56450cad3da0f032623
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-25 19:36:40 -0400

    Create AMBUSHHH.lua

---
+++ AMBUSHHH.lua
@@ -0,0 +1,40 @@
+---====== Define spawner ======---
+
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
+
+---====== Create entity ======---
+
+local Ambush = Spawner.createEntity({
+    CustomName = "Ambush",
+    Model = "https://github.com/UnavailableScripter/Doors-Models/blob/main/Ambush.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
+    Speed = 300,
+    MoveDelay = 2,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 150,
+    SpawnInFront = false,
+    ShatterLights = true,
+    FlickerLights = {
+        Enabled = false,
+        Duration = 1
+    },
+    Cycles = {
+        Min = 2,
+        Max = 6,
+        Delay = 0
+    },
+    CamShake = {
+        Enabled = true,
+        Values = {16, 2, 1, 0},
+        Range = 100
+    },
+    ResistCrucifix = false,
+    BreakCrucifix = true,
+    DeathMessage = {"You died to Ambush.", "You might need to hop in and out of your hiding spot a couple of times."},
+    IsCuriousLight = false
+})
+
+---====== Run entity ======---
+Spawner.runEntity(Ambush)
+
+--yay
